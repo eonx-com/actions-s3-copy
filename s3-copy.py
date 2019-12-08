c000b936729b868a851c89536581074f54e5777d
Damian Sloane <dddaaammmooo@hotmail.com>
2019-12-08 16:32:54 +1100

    Cleanup backup filename

--- s3-copy.py
+++ s3-copy.py
@@ -53,8 +53,10 @@ class S3:
                             'Key': destination_filename
                         }
 
-                        backup_filename = '{destination_filename}.{timestamp}'.format(
+                        backup_filename = '{destination_prefix}{destination_filename}{destination_suffix}.{timestamp}'.format(
                             destination_filename=destination_filename,
+                            destination_prefix=destination['prefix'],
+                            destination_suffix=destination['suffix'],
                             timestamp=datetime.utcnow().timestamp()
                         )
 
