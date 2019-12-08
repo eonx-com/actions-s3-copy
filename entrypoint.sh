9044ceece2627116d7289eda7aac76c155b6d50f
Damian Sloane <dddaaammmooo@hotmail.com>
2019-12-08 13:42:54 +1100

    Executable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
-# Get AWS credentials from workflow parameters
-export AWS_ACCESS_KEY_ID=${1}
-export AWS_SECRET_ACCESS_KEY=${2}
-export AWS_DEFAULT_REGION=${3}
-
-# Get other parameters from the workflow
-export SOURCE_PREFIX=${4}
-export SOURCE_SUFFIX=${5}
-export DESTINATION_BUCKET=${6}
-export DESTINATION_PREFIX=${7}
-export DESTINATION_SUFFIX=${8}
-export BACKUP_EXISTING=${9}
+# Get parameters from the workflow
+export SOURCE_PREFIX=${1}
+export SOURCE_SUFFIX=${2}
+export DESTINATION_BUCKET=${3}
+export DESTINATION_PREFIX=${4}
+export DESTINATION_SUFFIX=${5}
+export BACKUP_EXISTING=${6}
 
 cd /opt/s3-copy/
 pip install -r ./requirements.txt
